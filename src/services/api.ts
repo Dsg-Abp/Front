2890a32fb2f35b9871f73585b794c42eed1c3d48
Isaac - Exon <isaac.souza@exongenomics.com>
2024-08-21 10:16:28 -0300

    ajustes urls

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL: "https://back-production-d197.up.railway.app/",
+  baseURL: "https://back-production-d197.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
