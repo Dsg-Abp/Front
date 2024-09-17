9f00d1bb9815e07851495b5c22f1939ec81e19ab
MingRenan <100416045+MingRenan@users.noreply.github.com>
2024-09-16 21:34:02 -0300

    teste

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL: "http://localhost:3202/",
+  baseURL: "http://back-production-f64e.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
