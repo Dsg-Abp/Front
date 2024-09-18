0602fc99367ef2cf5fa3babf17616966982e9108
Isaac - Exon <isaac.souza@exongenomics.com>
2024-09-17 22:31:12 -0300

    ajuste de cores

--- src/components/ModalFuncionalidades/ModalCalendario.tsx
+++ src/components/ModalFuncionalidades/ModalCalendario.tsx
@@ -12,17 +12,19 @@ const ModalDetalhesDia: React.FC<ModalDetalhesDiaProps> = ({
   if (!diaSelecionado) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center">
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
+      {/* Filtro de fundo */}
       <div className="fixed inset-0 bg-black opacity-70"></div>
 
-      <div className=" w-full mx-2 relative bg-gradient-to-r from-[#a8f748] to-[#05fa29] p-6 rounded-lg shadow-lg text-center">
-        <h2 className="text-xl font-bold mb-4 text-black">
+      {/* Bloco centralizado */}
+      <div className="flex flex-col items-center w-[400px] mx-2 bg-blue-900 p-6 rounded-lg shadow-lg text-center relative text-white">
+        <h2 className="text-xl font-bold mb-4 text-white">
           {diaSelecionado.inicial} - {diaSelecionado.dia}
         </h2>
-        <p>dados aqui.</p>
+        <p className="mb-6">dados aqui.</p>
         <button
           onClick={fecharModal}
-          className="mt-6 bg-gradient-to-r from-[#979996] to-[#000000] hover:bg-gradient-to-r hover:from-[#000000] hover:to-[#979996] transition-colors text-white p-2 rounded"
+          className="bg-gradient-to-r from-[#979996] to-[#e41c09] hover:bg-gradient-to-r hover:from-[#000000] hover:to-[#979996] transition-colors text-white p-2 rounded"
         >
           Fechar
         </button>
