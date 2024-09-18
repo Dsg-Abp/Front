42e5a9d818bb9652533093e5a9c19c40f4e1f478
Isaac - Exon <isaac.souza@exongenomics.com>
2024-09-17 21:26:45 -0300

    ajuste no refrash das paginas

--- src/route/RouterProtect.tsx
+++ src/route/RouterProtect.tsx
@@ -1,10 +1,25 @@
-// ProtectedRoute.tsx
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 const ProtectedRoute: React.FC = () => {
   const { isAuthenticated } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      setIsLoading(true);
+
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      setIsLoading(false);
+    };
+
+    checkAuth();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
 };
