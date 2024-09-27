15874bd66c2a5fc54a35fcae9a114280be211bc5
MingRenan <100416045+MingRenan@users.noreply.github.com>
2024-09-27 20:05:29 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,16 @@
 import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
+<<<<<<< Updated upstream
 
   baseURL: "http://localhost:3202/",
+=======
+  baseURL: "http://localhost:3202",
+>>>>>>> Stashed changes
   headers: {
     "Content-Type": "application/json",
   },
-});
+});+
 
 api.interceptors.request.use(
   (config) => {
